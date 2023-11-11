377ab5059c819dbbdd73bbd47ae4d1b65a4b34fc
Moooover <billyrowan1980@gmail.com>
2023-11-11 12:13:35 +0100

    feat: add sharded db feature

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ use std::rc::Rc;
 use bytes::Bytes;
 use std::sync::{ Arc, Mutex };
 use std::collections::HashMap;
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
 
+const SHARD_COUNT: usize = 5;
 #[tokio::main]
 async fn main() {
   let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-  let db = Arc::new(Mutex::new(HashMap::new()));
+  let db = new_sharded_db(SHARD_COUNT); // Arc::new(Mutex::new(HashMap::new()));
 
   loop {
     let (socket, _) = listener.accept().await.unwrap();
@@ -26,7 +27,19 @@ async fn main() {
   }
 }
 
-async fn process(socket: TcpStream, db: Db) {
+fn new_sharded_db(num_shards: usize) -> ShardedDb {
+  let mut db = Vec::with_capacity(num_shards);
+  for _ in 0..num_shards {
+    db.push(Mutex::new(HashMap::new()))
+  }
+  Arc::new(db)
+}
+
+fn hash(key: String) -> usize {
+  key.len()
+}
+
+async fn process(socket: TcpStream, db: ShardedDb) {
 
   use mini_redis::Command::{self, Get, Set};
 
@@ -37,13 +50,13 @@ async fn process(socket: TcpStream, db: Db) {
   while let Some(frame) = connection.read_frame().await.unwrap() {
     let response = match Command::from_frame(frame).unwrap() {
       Set(cmd) => {
-        let mut db = db.lock().unwrap();
-        db.insert(cmd.key().to_string(), cmd.value().clone());
+        let mut shard = db[hash(cmd.key().to_string()) % SHARD_COUNT].lock().unwrap();
+        shard.insert(cmd.key().to_string(), cmd.value().clone());
         Frame::Simple("OK".to_string())
       }
       Get(cmd) => {
-        let db = db.lock().unwrap();
-        if let Some(value) = db.get(cmd.key()) {
+        let shard = db[hash(cmd.key().to_string()) % SHARD_COUNT].lock().unwrap();
+        if let Some(value) = shard.get(cmd.key()) {
           Frame::Bulk(value.clone().into())
         } else {
           Frame::Null
